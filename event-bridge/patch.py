ee8d51b29e87606175ae04ebc8b8110b60654cbd
Jacopo Rota <jacopo.r00ta@gmail.com>
2021-09-09 16:24:44 +0200

    Update patch.py

--- event-bridge/patch.py
+++ event-bridge/patch.py
@@ -7,7 +7,7 @@ def patch(current_version):
     data = f.read()
     f.close()
 
-    data = re.sub("newTag:.*", "newTag: " + current_version, data)
+    data = re.sub("newTag:.*", "newTag: " + current_version + "test", data)
 
     f = open('sandbox/kustomize/overlays/prod/kustomization.yaml', 'w')
     f.write(data)
