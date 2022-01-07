174e16d9e1e1ad3cb01f764371910d8082e35ef3
Jacopo Rota <jacopo.r00ta@gmail.com>
2022-01-07 13:38:44 +0100

    Update patch.py

--- event-bridge/patch.py
+++ event-bridge/patch.py
@@ -56,6 +56,16 @@ def patch(current_fleet_manager, current_fleet_shard, current_ingress, current_e
     with open('sandbox/kustomize/overlays/minikube/kustomization.yaml', 'w') as outfile:
         yaml.dump(prod_kustomization, outfile)
 
+    with open("sandbox/kustomize/overlays/minikube/shard/patches/deploy-config.yaml", "r") as stream:
+        try:
+            shard_patch = yaml.full_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+            sys.exit(1)
+
+    shard_patch['data']['EVENT_BRIDGE_INGRESS_IMAGE'] = "quay.io/5733d9e2be6485d52ffa08870cabdee0/ingress:" + current_ingress
+    shard_patch['data']['EVENT_BRIDGE_EXECUTOR_IMAGE'] = "quay.io/5733d9e2be6485d52ffa08870cabdee0/executor:" + current_executor
+
 if __name__ == "__main__":
     current_fleet_manager = sys.argv[1]
     current_fleet_shard = sys.argv[2]
