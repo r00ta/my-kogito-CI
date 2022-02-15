f4a6c0e325876f642437d8044661dbd2fc21db79
Jacopo Rota <jacopo.r00ta@gmail.com>
2022-02-15 09:12:36 +0100

    Update patch.py

--- patch.py
+++ patch.py
@@ -3,7 +3,7 @@ import sys
 import re
 
 def patch(next_version):
-    f = open('kogito-examples/trusty-demonstration/docker-compose/docker-compose.yml', 'r')
+    f = open('kogito-examples/kogito-quarkus-examples/trusty-demonstration/docker-compose/docker-compose.yml', 'r')
     data = f.read()
     f.close()
 
@@ -11,48 +11,48 @@ def patch(next_version):
     data = re.sub("kogito-trusty-infinispan:.*", "kogito-trusty-infinispan:" + next_version, data)
     data = re.sub("kogito-trusty-ui:.*", "kogito-trusty-ui:" + next_version, data)
 
-    f = open('kogito-examples/trusty-demonstration/docker-compose/docker-compose.yml', 'w')
+    f = open('kogito-examples/kogito-quarkus-examples/trusty-demonstration/docker-compose/docker-compose.yml', 'w')
     f.write(data)
     f.close()
 
-    f = open('kogito-examples/trusty-demonstration/kubernetes/README.md', 'r')
+    f = open('kogito-examples/kogito-quarkus-examples/trusty-demonstration/kubernetes/README.md', 'r')
     data = f.read()
     f.close()
 
     data = re.sub("KOGITO_VERSION=v.*", "KOGITO_VERSION=v" + next_version + ".0", data)
     data = re.sub("kogito-trusty-ui:.*", "kogito-trusty-ui:" + next_version, data)
 
-    f = open('kogito-examples/trusty-demonstration/kubernetes/README.md', 'w')
+    f = open('kogito-examples/kogito-quarkus-examples/trusty-demonstration/kubernetes/README.md', 'w')
     f.write(data)
     f.close()
 
-    f = open('kogito-examples/trusty-demonstration/kubernetes/resources/explainability.yaml', 'r')
+    f = open('kogito-examples/kogito-quarkus-examples/trusty-demonstration/kubernetes/resources/explainability.yaml', 'r')
     data = f.read()
     f.close()
 
     data = re.sub("kogito-explainability:.*", "kogito-explainability:" + next_version, data)
 
-    f = open('kogito-examples/trusty-demonstration/kubernetes/resources/explainability.yaml', 'w')
+    f = open('kogito-examples/kogito-quarkus-examples/trusty-demonstration/kubernetes/resources/explainability.yaml', 'w')
     f.write(data)
     f.close()
 
-    f = open('kogito-examples/trusty-demonstration/kubernetes/resources/trusty-ui.yaml', 'r')
+    f = open('kogito-examples/kogito-quarkus-examples/trusty-demonstration/kubernetes/resources/trusty-ui.yaml', 'r')
     data = f.read()
     f.close()
 
     data = re.sub("kogito-trusty-ui:.*", "kogito-trusty-ui:" + next_version, data)
 
-    f = open('kogito-examples/trusty-demonstration/kubernetes/resources/trusty-ui.yaml', 'w')
+    f = open('kogito-examples/kogito-quarkus-examples/trusty-demonstration/kubernetes/resources/trusty-ui.yaml', 'w')
     f.write(data)
     f.close()
 
-    f = open('kogito-examples/trusty-demonstration/kubernetes/resources/trusty.yaml', 'r')
+    f = open('kogito-examples/kogito-quarkus-examples/trusty-demonstration/kubernetes/resources/trusty.yaml', 'r')
     data = f.read()
     f.close()
 
     data = re.sub("kogito-trusty-infinispan:.*", "kogito-trusty-infinispan:" + next_version, data)
 
-    f = open('kogito-examples/trusty-demonstration/kubernetes/resources/trusty.yaml', 'w')
+    f = open('kogito-examples/kogito-quarkus-examples/trusty-demonstration/kubernetes/resources/trusty.yaml', 'w')
     f.write(data)
     f.close()
 
