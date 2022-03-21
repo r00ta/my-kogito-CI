1f2a136a29a5632527fe82656bd923e071f58010
Jacopo Rota <jacopo.r00ta@gmail.com>
2022-03-21 11:17:19 +0100

    fix

--- event-bridge/create_kafka_and_update_gists.sh
+++ event-bridge/create_kafka_and_update_gists.sh
@@ -17,17 +17,17 @@ cd sandbox
 
 rhoas kafka list | grep smart-events
 
-# if [ $? -ne 0 ]; then   
+if [ $? -ne 0 ]; then   
     # Clean up service accounts 
-~/bin/ocm login --token $OPENSHIFT_OFFLINE_TOKEN
-chmod +x ./dev/bin/troubleshoot/cleanup-service-accounts.sh
-./dev/bin/troubleshoot/cleanup-service-accounts.sh jrota
+    ~/bin/ocm login --token $OPENSHIFT_OFFLINE_TOKEN
+    chmod +x ./dev/bin/troubleshoot/cleanup-service-accounts.sh
+    ./dev/bin/troubleshoot/cleanup-service-accounts.sh jrota
+    
+    chmod +x ./dev/bin/kafka-setup.sh
+    ./dev/bin/kafka-setup.sh
 
-chmod +x ./dev/bin/kafka-setup.sh
-./dev/bin/kafka-setup.sh
-
-gh gist edit $SMART_EVENTS_GIST_ID -a ./dev/bin/credentials/smart-events.json
-gh gist edit $SMART_EVENTS_ADMIN_GIST_ID -a ./dev/bin/credentials/smart-events-admin.json
-gh gist edit $SMART_EVENTS_OPS_GIST_ID -a ./dev/bin/credentials/smart-events-ops.json
-gh gist edit $SMART_EVENTS_MC_GIST_ID -a ./dev/bin/credentials/smart-events-mc.json
-# fi
+    gh gist edit $SMART_EVENTS_GIST_ID -a ./dev/bin/credentials/smart-events.json
+    gh gist edit $SMART_EVENTS_ADMIN_GIST_ID -a ./dev/bin/credentials/smart-events-admin.json
+    gh gist edit $SMART_EVENTS_OPS_GIST_ID -a ./dev/bin/credentials/smart-events-ops.json
+    gh gist edit $SMART_EVENTS_MC_GIST_ID -a ./dev/bin/credentials/smart-events-mc.json
+fi
