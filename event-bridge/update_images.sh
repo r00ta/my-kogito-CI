4f4f38db5dd5f34684e7207462631d065be9a4f2
Jacopo Rota <jacopo.r00ta@gmail.com>
2022-08-04 09:52:11 +0200

    fix update smart events images

--- event-bridge/update_images.sh
+++ event-bridge/update_images.sh
@@ -6,9 +6,17 @@ LATEST_VERSION_MANAGER=$(python3 event-bridge/get_latest_image_version.py fleet-
 LATEST_VERSION_SHARD_OPERATOR=$(python3 event-bridge/get_latest_image_version.py fleet-shard)
 LATEST_VERSION_EXECUTOR=$(python3 event-bridge/get_latest_image_version.py executor)
 
+printf "Latest version for manager is: $LATEST_VERSION_MANAGER\n"
+printf "Latest version for shard operator is: $LATEST_VERSION_SHARD_OPERATOR\n"
+printf "Latest version for executor is: $LATEST_VERSION_EXECUTOR\n"
+
 COMMIT_VERSION_MANAGER=$(printf $LATEST_VERSION_MANAGER | grep -Eio '[A-Za-z0-9]{8,}' -m 1)
 COMMIT_VERSION_SHARD_OPERATOR=$(printf $LATEST_VERSION_SHARD_OPERATOR | grep -Eio '[A-Za-z0-9]{8,}' -m 1)
 COMMIT_VERSION_EXECUTOR=$(printf $LATEST_VERSION_EXECUTOR | grep -Eio '[A-Za-z0-9]{8,}' -m 1)
+
+printf "Found commit $COMMIT_VERSION_MANAGER for manager\n"
+printf "Found commit $COMMIT_VERSION_SHARD_OPERATOR for shard operator\n"
+printf "Found commit $COMMIT_VERSION_EXECUTOR for executor\n"
 
 git clone git@github.com:r00ta/sandbox.git
 
@@ -18,9 +26,13 @@ git fetch upstream
 
 git merge upstream/main
 
-MANAGER=$(git log -1 --format='%ci' $COMMIT_VERSION_MANAGER | date --utc)
-SHARD=$(git log -1 --format='%ci' $COMMIT_VERSION_SHARD_OPERATOR | date --utc)
-EXECUTOR=$(git log -1 --format='%ci' $COMMIT_VERSION_EXECUTOR | date --utc)
+MANAGER=$(date -d @$(git log -n1 --format="%at" $COMMIT_VERSION_MANAGER) +%Y%m%d%H%M)
+SHARD=$(date -d @$(git log -n1 --format="%at" $COMMIT_VERSION_SHARD_OPERATOR) +%Y%m%d%H%M)
+EXECUTOR=$(date -d @$(git log -n1 --format="%at" $COMMIT_VERSION_EXECUTOR) +%Y%m%d%H%M)
+
+printf "Commit date for latest manager is $MANAGER\n"
+printf "Commit date for latest shard operator is $SHARD\n"
+printf "Commit date for latest executor is $EXECUTOR\n"
 
 if [[ $MANAGER > $SHARD ]]
 then
@@ -37,11 +49,14 @@ else
         TO_USE=$COMMIT_VERSION_EXECUTOR
 fi
 
+printf "Most recent commit is $TO_USE"
+
 TAG=$(git rev-parse --short HEAD)
 SHORT_TAG=${TAG:0:5}
 
 JIRA=$(git log --format=%B -n 1 $TO_USE | grep -Ei 'MGDOBR-[0-9]+' -m 1)
 
+printf "Retrieved jira is $JIRA"
 OUT=$(gh pr list)
 
 if [[ "$OUT" =~ .*"$JIRA - Update kustomization images".* ]]; then
