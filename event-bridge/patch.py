21e4032f4a4293cead4f904cc7e9f556dfa16449
Karel Suta <krlsuta@gmail.com>
2022-08-22 18:10:05 +0200

    Migrate E2E tests PR check to Red Hat SSO
    Adjusting to comply with https://github.com/5733d9e2be6485d52ffa08870cabdee0/sandbox/pull/1051

--- event-bridge/patch.py
+++ event-bridge/patch.py
@@ -36,26 +36,26 @@ def patch(current_fleet_manager, current_fleet_shard, current_executor):
     with open('sandbox/kustomize/base-openshift/shard/patches/deploy-config.yaml', 'w') as outfile:
         yaml.dump(shard_patch, outfile)
 
-    # minikube overlay
-    with open("sandbox/kustomize/overlays/minikube/kustomization.yaml", "r") as stream:
+    # ci overlay
+    with open("sandbox/kustomize/overlays/ci/kustomization.yaml", "r") as stream:
         try:
-            minikube_kustomization = yaml.full_load(stream)
+            ci_kustomization = yaml.full_load(stream)
         except yaml.YAMLError as exc:
             print(exc)
             sys.exit(1)
 
     # Manager
-    manager = next(filter(lambda x: x['name'] == 'event-bridge-manager', minikube_kustomization['images']))
+    manager = next(filter(lambda x: x['name'] == 'event-bridge-manager', ci_kustomization['images']))
     manager['newTag'] = current_fleet_manager
 
     # Shard
-    shard = next(filter(lambda x: x['name'] == 'event-bridge-shard-operator', minikube_kustomization['images']))
+    shard = next(filter(lambda x: x['name'] == 'event-bridge-shard-operator', ci_kustomization['images']))
     shard['newTag'] = "k8s-" + current_fleet_shard + "-jvm"
 
-    with open('sandbox/kustomize/overlays/minikube/kustomization.yaml', 'w') as outfile:
-        yaml.dump(minikube_kustomization, outfile)
+    with open('sandbox/kustomize/overlays/ci/kustomization.yaml', 'w') as outfile:
+        yaml.dump(ci_kustomization, outfile)
 
-    with open("sandbox/kustomize/overlays/minikube/shard/patches/deploy-config.yaml", "r") as stream:
+    with open("sandbox/kustomize/overlays/ci/shard/patches/deploy-config.yaml", "r") as stream:
         try:
             shard_patch = yaml.full_load(stream)
         except yaml.YAMLError as exc:
@@ -64,7 +64,7 @@ def patch(current_fleet_manager, current_fleet_shard, current_executor):
 
     shard_patch['data']['EVENT_BRIDGE_EXECUTOR_IMAGE'] = "quay.io/5733d9e2be6485d52ffa08870cabdee0/executor:" + current_executor
 
-    with open('sandbox/kustomize/overlays/minikube/shard/patches/deploy-config.yaml', 'w') as outfile:
+    with open('sandbox/kustomize/overlays/ci/shard/patches/deploy-config.yaml', 'w') as outfile:
         yaml.dump(shard_patch, outfile)
         
 if __name__ == "__main__":
